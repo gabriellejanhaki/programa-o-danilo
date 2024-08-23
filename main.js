ad5690447e7d74e020ca47928989043e84c280ac
gabriellejanhaki <106555870+gabriellejanhaki@users.noreply.github.com>
2024-08-23 11:54:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 const botoes = document.querySelectorAll(botao);
 console.log(botoes)
 for (let i=0; i < botões.length; i++) { console.log(i); }
-
+const textos = document.querySelectorAll(aba-conteudo);
+textos[i].classList.add(ativo);
+textos[j].classList.remove(ativo);
+const contadores = document.querySelectorAll(contador);
+const contadores = document.querySelectorAll(contador)
+const tempoObjetivo1 = new Date;
+let tempoAtual = new Date();
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const contadores = document.querySelectorAll(contador);
+contadores[0]
+contadores[0].textContent = Contagem;
+function calculaTempo(tempoObjetivo){
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+return dias  + dias + horas + horas + minutos + minutos + segundos + segundos;
+ }
